e724fe14131c396e78e1aa074ec16adb25fb130e
jinjero <jintojero@gmail.com>
2024-02-28 04:45:08 +0900

    refactor: Modify code
    #4

--- model/data/sec2frame.py
+++ model/data/sec2frame.py
@@ -3,9 +3,15 @@ import cv2
 import pandas as pd
 
 
-def extractor(video_path, csv_path):
-    csv_data = pd.read_csv(csv_path)
+def main(video_path, anno_csv_path, save_csv_path):
+    csv_data = pd.read_csv(anno_csv_path)
     file_names = [i.strip('[]"') for i in csv_data["file_list"]]
+    start_list = csv_data["temporal_segment_start"]
+    end_list = csv_data["temporal_segment_end"]
+
+    # second
+    start_sec = [(int(round(s))) for s in start_list]
+    end_sec = [(int(round(e))) for e in end_list]
 
     # fps
     fps = {}
@@ -18,11 +24,11 @@ def extractor(video_path, csv_path):
     # frame
     start_frame = [
         (int(round(start) * fps[name]))
-        for name, start in zip(file_names, csv_data["temporal_segment_start"])
+        for name, start in zip(file_names, start_list)
     ]
     end_frame = [
         (int(round(end) * fps[name]))
-        for name, end in zip(file_names, csv_data["temporal_segment_end"])
+        for name, end in zip(file_names, end_list)
     ]
 
     # class
@@ -35,24 +41,26 @@ def extractor(video_path, csv_path):
         elif data == '{"Shoplifting":"2"}':
             class_names.append("Background")
 
-    return file_names, start_frame, end_frame, class_names
+    # csv
+    df = pd.DataFrame(
+        {
+            "file_name": video,
+            "start_sec": start_sec,
+            "end_sec": end_sec,
+            "start_frame": start_frame,
+            "end_frame": end_frame,
+            "class": class_names,
+        }
+    )
+
+    df.to_csv(save_csv_path, index=False)
 
 
-video_path = (
-    "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/videos"
-)
-csv_path = "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/annotation.csv"
-save_csv = "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/anno_frame.csv"
+if __name__ == "__main__":
+    video_path = (
+        "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/videos"
+    )
+    anno_csv_path = "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/annotation.csv"
+    save_csv_path = "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/dataset/anno_frame.csv"
 
-video, start, end, class_ = extractor(video_path, csv_path)
-
-df = pd.DataFrame(
-    {
-        "file_name": video,
-        "start_frame": start,
-        "end_frame": end,
-        "class": class_,
-    }
-)
-
-df.to_csv(save_csv, index=False)
+    main(video_path, anno_csv_path, save_csv_path)
