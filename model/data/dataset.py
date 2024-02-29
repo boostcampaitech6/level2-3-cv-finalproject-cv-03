d39a724957e1eddfe345578b33548e79541d5cbc
jinjero <jintojero@gmail.com>
2024-02-29 22:49:37 +0900

    fix: Fix frame extraction error
    #13

--- model/data/dataset.py
+++ model/data/dataset.py
@@ -6,33 +6,37 @@ from torch.utils.data import Dataset
 
 
 class TrainDataset(Dataset):
-    def __init__(self, frame_num, video_path, anno_csv_path, transforms=None):
+    def __init__(
+        self, frame_num, video_dir_path, anno_csv_path, transforms=None
+    ):
         csv_data = pd.read_csv(anno_csv_path)
 
-        self.video = [
-            os.path.join(video_path, i) for i in csv_data["clip_fname"]
+        self.video_paths = [
+            os.path.join(video_dir_path, fname)
+            for fname in csv_data["clip_fname"]
         ]
-        self.label = [
-            i.apply(lambda x: x.split("_")[0]) for i in csv_data["clip_fname"]
+        self.video_labels = [
+            fname.split("_")[0] for fname in csv_data["clip_fname"]
         ]
         self.frame_num = frame_num
         self.transforms = transforms
 
     def __len__(self):
-        return len(self.video)
+        return len(self.video_paths)
 
     def __getitem__(self, item):
         frames = []
-        labels = []
 
-        video_path = self.video[item]
-        video_read = cv2.VideoCapture(video_path)
-        video_frame = int(video_read.get(cv2.CAP_PROP_FRAME_COUNT))
-        cut_frame = max(int(video_frame / self.frame_num), 1)
+        video_path = self.video_paths[item]
+        video = cv2.VideoCapture(video_path)
+        video_frame_num = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        interval = max(round(int(video_frame_num / self.frame_num)), 1)
 
-        for frame_idx in range(frame_num, video_frame, cut_frame):
-            video_read.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
-            success, frame = video_read.read()
+        for frame_idx in range(
+            0, min(video_frame_num, self.frame_num * interval), interval
+        ):
+            video.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+            success, frame = video.read()
 
             if not success:
                 break
@@ -42,18 +46,22 @@ class TrainDataset(Dataset):
             normalized_frame = normalized_frame.astype(np.float32)
             frames.append(normalized_frame)
 
-        video_read.release()
+        video.release()
+
+        while len(frames) < self.frame_num:
+            if video_frame_num < self.frame_num:
+                frames.append(frames[-1])
 
         frames = np.array(frames)
         frames = np.transpose(frames, (0, 3, 1, 2))
 
-        labels = [self.label[item] for _ in range(frame_num)]
+        labels = [self.video_labels[item] for _ in range(self.frame_num)]
 
-        return frames, labels, video_path
+        return frames, labels
 
 
 frame_num = 64
-video_path = "../dataset/videos"
+video_dir_path = "../dataset/videos"
 anno_csv_path = "../dataset/metadata.csv"
 
-dataset = TrainDataset(frame_num, video_path, anno_csv_path)
+dataset = TrainDataset(frame_num, video_dir_path, anno_csv_path)
