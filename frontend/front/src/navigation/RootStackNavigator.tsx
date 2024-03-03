e99169742e47b2fff181b3d0c442d70502e76d4e
Jungtaxi <beagentleman7@gmail.com>
2024-03-03 23:54:01 +0900

    feat: Add navigator
    #23

--- frontend/front/src/navigation/RootStackNavigator.tsx
+++ frontend/front/src/navigation/RootStackNavigator.tsx
@@ -1,4 +1,3 @@
-// src/navigation/RootStackNavigator.tsx
 import * as React from 'react';
 import { Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,36 +7,48 @@ import BottomTabNavigator from './BottomTabNavigator';
 import Tab1Screen from '../screens/Tab1/Tab1Screen';
 import Tab2Screen from '../screens/Tab2/Tab2Screen';
 import Tab3Screen from '../screens/Tab3/Tab3Screen';
-import { TouchableOpacity } from 'react-native';
+import LogDetailScreen from "../screens/Tab1/LogDetailScreen";
 import { useNavigation } from '@react-navigation/native';
 
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+  Home: undefined;
+  Tab1Screen: undefined;
+  Tab2Screen: undefined;
+  Tab3Screen: undefined;
+  LogDetailScreen: {   
+    log_id: number;
+    anomaly_create_time: string;
+    anomaly_save_path: string;
+    cctv_id: number;
+    cctv_name: string;
+    cctv_url: string; 
+  };
+};
 
-
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
   const navigation = useNavigation();
 
   return (
-    <Stack.Navigator initialRouteName="SignIn" screenOptions = {{ headerShown: false }}>
-      <Stack.Screen name="SignIn" component={SignInScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen 
-        name="Home"
-        component={BottomTabNavigator}
-        options={{ 
-          headerLeft: () => (
-            // <TouchableOpacity onPress={() => navigation.navigate('SignInScreen')}>
-              <Image source={require('../../assets/Logo.png')} style={{ width: 50, height: 50 }} />
-            // </TouchableOpacity>
-          ), 
-          headerShown: true,
-          headerTitle: '',
-        }}
-      />
-      <Stack.Screen name="Tab1Screen" component={Tab1Screen} />
-      <Stack.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Stack.Screen name="Tab3Screen" component={Tab3Screen} />
+    <Stack.Navigator initialRouteName="SignIn" 
+      screenOptions = {{ 
+        headerShown: true,
+        headerTitle: '가디언아이즈',
+        headerLeft: () => (
+          <Image
+            source={require('../../assets/Logo.png')}
+            style={{ width: 50, height: 50 }}
+          />)}}>
+    <Stack.Screen name="SignIn" component={SignInScreen} />
+    <Stack.Screen name="SignUp" component={SignUpScreen} />
+    <Stack.Screen name="Home" component={BottomTabNavigator}/>
+    <Stack.Screen name="Tab1Screen" component={Tab1Screen} />
+    <Stack.Screen name="Tab2Screen" component={Tab2Screen} />
+    <Stack.Screen name="Tab3Screen" component={Tab3Screen} />
+    <Stack.Screen name="LogDetailScreen" component={LogDetailScreen} />
     </Stack.Navigator>
   );
 }
