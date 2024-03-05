36f40cdfe7b2569d73aa7581b589c0b97c45b979
Ryan Lee <rsl82@rutgers.edu>
2024-03-05 12:02:24 +0900

    feat: Add backend code
    #40

---
+++ backend/app/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+from app.core.config import settings
+from app.db.database import Base, engine
+from app.api import (
+    router,
+    memberRouter,
+    cctvRouter,
+    streamingRouter,
+    settingRouter,
+)
+from app.db.models import *
+from app.middleware import TimeHeaderMiddleware
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 데이터베이스 테이블 생성
+    # logging.basicConfig()
+    # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+def get_application():
+    _app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+
+    _app.add_middleware(
+        CORSMiddleware,
+        # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    _app.include_router(router)
+    _app.include_router(memberRouter)
+    _app.include_router(cctvRouter)
+    _app.include_router(streamingRouter)
+    _app.include_router(settingRouter)
+    _app.add_middleware(TimeHeaderMiddleware)
+
+    return _app
+
+
+app = get_application()
---
+++ backend/app/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+
+class CustomBaseModel(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class MemberBase(CustomBaseModel):
+    email: str
+    password: str
+    member_name: Optional[str]
+    store_name: str
+
+
+class MemberDetail(MemberBase):
+    member_id: int
+    threshold: float
+    save_time_length: int
+    cctv_info_yn: bool
+    create_time: datetime
+    update_time: Optional[datetime]
+
+
+class CCTVBase(CustomBaseModel):
+    cctv_id: int
+    member_id: int
+    cctv_name: str
+    cctv_url: str
+
+
+class LogBase(CustomBaseModel):
+    log_id: int
+    cctv_id: int
+    anomaly_score: float
+    anomaly_save_path: str
+    anomaly_feedback: int
+    anomaly_delete_yn: bool
