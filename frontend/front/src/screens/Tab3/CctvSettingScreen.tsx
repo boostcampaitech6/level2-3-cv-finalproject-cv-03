f7e6de8369b8a2b744a59f767205b5fdde719a03
Jungtaxi <beagentleman7@gmail.com>
2024-03-08 02:53:41 +0900

    feat: Init design for frame7
    - url 주소가 너무 길어질 경우, 알아서 말줄임 표시 '...' 하겠끔 설정
    - add 버튼 추가
    - url 고유 아이디에 대한 요소 나열
    
    #55

--- frontend/front/src/screens/Tab3/CctvSettingScreen.tsx
+++ frontend/front/src/screens/Tab3/CctvSettingScreen.tsx
@@ -1,78 +1,92 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, Switch, TouchableOpacity, Alert, BackHandler } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, StyleSheet, FlatList, Switch, TouchableOpacity, Alert, BackHandler } from 'react-native';
 import { Text } from "galio-framework";
 import { Images, argonTheme } from "../../constants";
 import { NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../navigation/RootStackNavigator';
+import { UserContext } from '../../UserContext';
+
+interface Cctvlist {
+  cctv_id: number,
+  cctv_name: string,
+  cctv_url: string,
+}
 
 type Props = {
   navigation: NavigationProp<RootStackParamList, 'CctvSettingScreen'>;
 };
 
 export default function CctvSettingScreen({ navigation }: Props) {
-  const [isHighAlert, setIsHighAlert] = React.useState(false);
-  const toggleSwitch = () => setIsHighAlert(previousState => !previousState);
+  const { user } = useContext(UserContext);
+  const [Cctvlists, setCctvlists] = useState<Cctvlist[]>([]);
 
   useEffect(() => {
-    const backAction = () => {
-      Alert.alert('Hold on!', 'Are you sure you want to go back?', [
-        {
-          text: 'Cancel',
-          onPress: () => null,
-          style: 'cancel',
-        },
-        { text: 'YES', onPress: () => navigation.goBack() },
-      ]);
-      return true;
+    const fetchAnomalyEvents = async () => {
+      try {
+        const response = await fetch(`http://10.28.224.142:30016/api/v0/settings/cctv_list_lookup?member_id=${user}`, {
+          method: "GET",
+          headers: { 'accept': 'application/json' },
+          });
+        console.log('receving data...');
+        const data = await response.json();
+        console.log(response.ok)
+  
+        if (response.ok) {
+          console.log(data.isSuccess);
+          console.log(data.result);
+          setCctvlists(data.result);
+        } else {
+          console.error('API 호출에 실패했습니다:', data);
+        }
+      } catch (error) {
+        console.error('API 호출 중 예외가 발생했습니다:', error);
+      }
     };
+    fetchAnomalyEvents();
+  }, []);
 
-  
-      // Add back handler event listener
-      const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
-  
-      // Remove event listener on cleanup
-      return () => backHandler.remove();
-    }, [navigation]);
-
-    return (
-      <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>CCTV 상세 화면</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isHighAlert ? "#f5dd4b" : "#f4f3f4"}
-          onValueChange={toggleSwitch}
-          value={isHighAlert}
-        />
+  const renderItem = ({ item }: { item: Cctvlist }) => (
+    <View style={styles.item}>
+      <View style={styles.item_header}>
+        <Text style={styles.itemHeaderText}>{item.cctv_name}</Text>
+        <Text style={styles.urlText} numberOfLines={1} ellipsizeMode='tail'>
+          {item.cctv_url}
+        </Text>
       </View>
-      <View style={styles.cctvContainer}>
-        {/* CCTV 영상 배치 */}
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.detailText}>이름</Text>
-        <View style={styles.middle}>
-          <TouchableOpacity style={styles.feedback_button}>
-            <Text style={styles.buttonText}>피드백 남기기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.download_button}>
-            <Text style={styles.buttonText}>영상 다운로드</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.delete_button}>
-            <Text style={styles.buttonText}>기록 삭제하기</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.footer}>
-        <TouchableOpacity 
-          style={styles.back_button}
-          onPress={() => {navigation.goBack();
-        }}>
-          <Text style={styles.buttonText}>이전</Text>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.feedback_button}>
+          <Text style={styles.buttonText}>수정</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.delete_button}>
+          <Text style={styles.buttonText}>삭제</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
-  };
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          ListHeaderComponent={
+            <View style={styles.header}>
+            <Text style={styles.headerText}>CCTV 세팅</Text>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => {
+                // TODO: Implement add CCTV functionality
+              }}
+            >
+              <Text style={styles.addButtonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+          }
+          data={Cctvlists}
+          renderItem={renderItem}
+          keyExtractor={item => item.cctv_id.toString()}
+          style={{ flex: 1 }}
+        />
+      </View>
+  );};
   
   const styles = StyleSheet.create({
     container: {
@@ -84,72 +98,83 @@ export default function CctvSettingScreen({ navigation }: Props) {
       justifyContent: 'space-between',
       alignItems: 'center',
       padding: 20,
-      backgroundColor: '#fff',
+    },
+    item_header: {
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+      padding: 20,
+      flex: 2
     },
     headerText: {
       fontSize: 20,
       fontWeight: 'bold',
       fontFamily: 'SG',
     },
-    cctvContainer: {
-      flex: 1,
-      // CCTV 이미지를 위한 스타일
+    itemHeaderText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      fontFamily: 'NGB',
     },
-    details: {
-      padding: 20,
-      backgroundColor: '#fff',
-    },
-    detailText: {
+    urlText: {
       fontSize: 16,
       marginVertical: 4,
       fontFamily: 'NGB',
+      flexShrink: 1,
     },
-    footer: {
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
-      paddingVertical: 20,
-      backgroundColor: '#fff',
-      bottom: 0,
-    },
-    middle: {
-      paddingVertical: 20,
-      flexDirection: 'row',
+    buttons: {
+      paddingVertical: 10,
+      flexDirection: 'column',
       justifyContent: 'space-around',
+      flex:1
     },
     feedback_button: {
       padding: 10,
+      marginVertical: 3,
       backgroundColor: argonTheme.COLORS.SUCCESS,
       borderRadius: 5,
       flex: 1,
       marginHorizontal: 10,
     },
-    download_button: {
-      padding: 10,
-      backgroundColor: argonTheme.COLORS.GRADIENT_START,
-      borderRadius: 5,
-      flex: 1,
-      marginHorizontal: 10,
-    },  
     delete_button: {
       padding: 10,
+      marginVertical: 3,
       backgroundColor: argonTheme.COLORS.LABEL,
       borderRadius: 5,
       flex: 1,
       marginHorizontal: 10,
     },    
-    back_button: {
-      padding: 10,
-      backgroundColor: argonTheme.COLORS.ACTIVE,
-      borderRadius: 5,
-      width: 120,
-      height: 40,
-      marginHorizontal: 20,
-    },
     buttonText: {
       color: '#fff',
       fontSize: 16,
       alignContent: 'center',
       textAlign: 'center',
       fontFamily: 'NGB',
+    },
+    addButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: argonTheme.COLORS.PLACEHOLDER,
+      borderRadius: 25,
+      width: 50,
+      height: 50
+    },
+    addButtonText: {
+      fontSize: 43,
+      lineHeight: 50,
+      color: argonTheme.COLORS.ICON,
+      fontWeight: 'bold'
+    },
+    item: {
+      backgroundColor: '#f0f0f0',
+      borderWidth: 1,
+      borderColor: '#CCCCCC',
+      borderRadius: 10,
+      padding: 10,
+      marginVertical: 10,
+      marginHorizontal: 20,
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
     },
   });
