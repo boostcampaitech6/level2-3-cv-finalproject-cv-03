aa30ae26f693414a3c160454f145e722ae0deadb
kyungeun <151593264+rudeuns@users.noreply.github.com>
2024-03-11 01:00:18 +0900

    fix: Remove label tracker (#75)
    #74

--- model/trainer/valid_loop.py
+++ model/trainer/valid_loop.py
@@ -2,17 +2,14 @@ import torch
 from tqdm import tqdm
 
 
-def valid_loop(
-    model, valid_loader, criterion, metric_tracker, label_tracker, device
-):
+def valid_loop(model, valid_loader, criterion, metric_tracker, device):
     model.to(device)
     model.eval()
 
     metric_tracker.reset()
-    label_tracker.reset()
 
     with torch.no_grad():
-        for frames, labels, file_names in tqdm(valid_loader, desc="[Valid]"):
+        for frames, labels in tqdm(valid_loader, desc="[Valid]"):
             frames = frames.to(device, non_blocking=True)
             labels = labels.to(device, non_blocking=True)
 
@@ -24,6 +21,5 @@ def valid_loop(
             preds = preds.cpu().numpy()
 
             metric_tracker.update(loss=loss.item(), true=labels, pred=preds)
-            label_tracker.update(file_names=file_names, labels=preds)
 
-    return metric_tracker.result(), label_tracker.result()
+    return metric_tracker.result()
