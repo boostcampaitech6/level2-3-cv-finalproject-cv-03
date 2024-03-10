d8d6f5f08e4e8c5abd458586ad38ba576209f774
kyungeun <151593264+rudeuns@users.noreply.github.com>
2024-03-10 20:55:35 +0900

    feat: Add validation code (#67)
    * feat: Add valid dataset class
    
    #66
    
    * feat: Add valid loop
    
    #66
    
    * feat: Add valid utils
    
    #66
    
    * feat: Add init file
    
    #66
    
    * feat: Add sampling valid frame logic
    
    #66
    
    * feat: Add making valid csv file
    
    #66
    
    * refactor: Rename file
    
    #66

---
+++ model/data/sample_valid_frame.py
@@ -0,0 +1,93 @@
+import os
+import numpy as np
+import pandas as pd
+import cv2
+import albumentations as A
+
+
+def main(params, paths):
+    anno_df = pd.read_csv(paths["anno_path"])
+    new_anno_dict = {"file_name": [], "class": [], "pred_t": []}
+
+    for _, row in anno_df.iterrows():
+        file_name = row["file_name"]
+        label = row["label"]
+
+        video_path = os.path.join(paths["video_dir_path"], file_name)
+        video = cv2.VideoCapture(video_path)
+
+        fps = int(video.get(cv2.CAP_PROP_FPS))
+        total_frame = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        total_len = int(total_frame / fps)
+
+        for t in range(total_len - params["clip_len"] + 1):
+            indices = np.linspace(
+                t * fps,
+                (t + params["clip_len"]) * fps - 1,
+                params["clip_frame"],
+            )
+            indices = np.round(indices).astype(int)
+
+            frames = []
+            for idx in indices:
+                video.set(cv2.CAP_PROP_POS_FRAMES, idx)
+                success, frame = video.read()
+
+                if not success:
+                    break
+
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = A.Resize(params["frame_size"], params["frame_size"])(
+                    image=frame
+                )["image"]
+                frames.append(frame)
+
+            frames = np.transpose(np.array(frames), (0, 3, 1, 2))
+            np.save(
+                os.path.join(
+                    paths["save_dir_path"],
+                    f"{os.path.splitext(file_name)[0]}_{t}.npy",
+                ),
+                frames,
+            )
+
+            sample_labels = label[t : t + params["clip_len"]]
+
+            if sample_labels[-1] == 1:
+                clip_class = 1
+            elif np.any(sample_labels == 1):
+                clip_class = 2
+            elif np.any(sample_labels == 0):
+                clip_class = 0
+            else:
+                clip_class = 3
+
+            new_anno_dict["file_name"].append(
+                f"{os.path.splitext(file_name)[0]}_{t}.npy"
+            )
+            new_anno_dict["class"].append(clip_class)
+            new_anno_dict["pred_t"].append(t + params["clip_len"])
+
+        video.release()
+
+    new_anno_df = pd.DataFrame(new_anno_dict)
+    new_anno_df.to_csv(paths["new_anno_path"], index=False)
+
+
+if __name__ == "__main__":
+    params = {
+        "clip_len": 5,
+        "clip_frame": 16,
+        "frame_size": 224,
+    }
+    paths = {
+        "anno_path": "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/model/dataset/valid/annotation.csv",
+        "video_dir_path": "/data/ephemeral/home/level2-3-cv-finalproject-cv-03/model/dataset/valid/videos",
+        "save_dir_path": f"/data/ephemeral/home/level2-3-cv-finalproject-cv-03/model/dataset/valid/frames/T{params['clip_len']}_F{params['clip_frame']}_S{params['frame_size']}",
+        "new_anno_path": f"/data/ephemeral/home/level2-3-cv-finalproject-cv-03/model/dataset/valid/annotation_t{params['clip_len']}_f{params['clip_frame']}_s{params['frame_size']}.csv",
+    }
+
+    if not os.path.exists(paths["save_dir_path"]):
+        os.makedirs(paths["save_dir_path"])
+
+    main(params, paths)
---
+++ model/trainer/utils.py
@@ -0,0 +1,71 @@
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+)
+
+
+class MetricTracker:
+    def __init__(self):
+        self.loss = {"total": None, "count": None}
+        self.true = None
+        self.pred = None
+
+        self.reset()
+
+    def reset(self):
+        self.loss["total"] = 0
+        self.loss["count"] = 0
+        self.true = []
+        self.pred = []
+
+    def update(self, loss=None, true=None, pred=None):
+        assert all(
+            x is not None for x in [loss, true, pred]
+        ), "Warning: None exists."
+
+        self.loss["total"] += loss
+        self.loss["count"] += 1
+        self.true.extend(true)
+        self.pred.extend(pred)
+
+    def result(self):
+        scores = {
+            "loss": None,
+            "precision": None,
+            "recall": None,
+            "f1": None,
+            "confusion_matrix": None,
+        }
+
+        scores["loss"] = self.loss["total"] / self.loss["count"]
+        scores["precision"] = precision_score(
+            self.true, self.pred, average=None, zero_division=0.0
+        )
+        scores["recall"] = recall_score(
+            self.true, self.pred, average=None, zero_division=0.0
+        )
+        scores["f1"] = f1_score(
+            self.true, self.pred, average=None, zero_division=0.0
+        )
+        scores["confusion_matrix"] = confusion_matrix(self.true, self.pred)
+
+        return scores
+
+
+class LabelTracker:
+    def __init__(self):
+        self.labels = None
+
+        self.reset()
+
+    def reset(self):
+        self.labels = {}
+
+    def update(self, file_names, labels):
+        for file_name, label in zip(file_names, labels):
+            self.labels[file_name] = label
+
+    def result(self):
+        return self.labels
