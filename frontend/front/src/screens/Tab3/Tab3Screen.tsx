49e1ee5c6d25f2a934647596abc40ac5f0a691aa
Jungtaxi <beagentleman7@gmail.com>
2024-03-12 03:18:00 +0900

    chore: Apply pre-commit
    #70

--- frontend/front/src/screens/Tab3/Tab3Screen.tsx
+++ frontend/front/src/screens/Tab3/Tab3Screen.tsx
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet, Dimensions, TouchableOpacity, ImageBackground } from 'react-native';
-import { Images, argonTheme } from "../../constants";
-import { NavigationProp } from '@react-navigation/native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Images } from "../../constants";
+import { NavigationProp } from "@react-navigation/native";
 
+type RootStackParamList = {
+  CctvSettingScreen: undefined;
+  Alarm: undefined;
+  Profile: undefined;
+};
 
 interface Tab2ScreenProps {
-  navigation: NavigationProp<any>;
+  navigation: NavigationProp<
+    RootStackParamList,
+    "CctvSettingScreen" | "Alarm" | "Profile"
+  >;
 }
 
 const { width, height } = Dimensions.get("screen");
@@ -18,52 +33,52 @@ export default function Tab2Screen(props: Tab2ScreenProps) {
       style={{ width, height, zIndex: 1 }}
     >
       <View style={styles.container}>
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('CctvSettingScreen', {})}
+          onPress={() => navigation.navigate("CctvSettingScreen", {})}
         >
           <Text style={styles.buttonText}>CCTV</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Alarm', {})}
+          onPress={() => navigation.navigate("Alarm", {})}
         >
           <Text style={styles.buttonText}>알림/동영상</Text>
         </TouchableOpacity>
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Profile', {})}
+          onPress={() => navigation.navigate("Profile", {})}
         >
           <Text style={styles.buttonText}>개인 정보</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
   );
-};
-  
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    backgroundColor: '#FFFFFF', // White color for the button background
+    backgroundColor: "#FFFFFF", // White color for the button background
     padding: 20,
     marginVertical: 10,
     borderRadius: 10,
-    shadowColor: 'rgba(0,0,0, .4)', // Shadow color
+    shadowColor: "rgba(0,0,0, .4)", // Shadow color
     shadowOffset: { height: 1, width: 1 },
     shadowOpacity: 1,
     shadowRadius: 1,
     elevation: 2,
-    width: '80%', // Set width to 80% of the container width
+    width: "80%", // Set width to 80% of the container width
   },
   buttonText: {
     fontSize: 18,
-    color: '#000', // Black color for the text
-    textAlign: 'center',
+    color: "#000", // Black color for the text
+    textAlign: "center",
     fontFamily: "NGB",
   },
 });
