676d25b51d68336744874dd347abec2cf37ca6e0
김정택_T6038 <beagentleman7@gmail.com>
2024-03-12 22:11:24 +0900

    feat: Add  pre-commit in js code (#87)
    * chore: Apply pre-commit
    
    - add husky eslint prettier lint-staged auto-husky in Dev-dependencies
    - install husky
    
    #70
    
    * chore: Apply pre-commit
    
    - add husky eslint prettier lint-staged auto-husky in Dev-dependencies
    - install husky
    
    #70
    
    * feat: Add padding bottom
    
    - add padding bottom to Tab1Screen
    - add padding bottom to CctvSettingScreen
    
    #70
    
    * chore: Apply pre-commit
    
    #70
    
    * feat: change background image in Tab3
    
    #70
    
    * chore: Apply pre-commit
    
    #70

--- frontend/front/src/screens/Tab1/Tab1Screen.tsx
+++ frontend/front/src/screens/Tab1/Tab1Screen.tsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, FlatList, StyleSheet, TouchableOpacity, TextInput, Dimensions, ImageBackground } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
-import { UserContext } from '../../UserContext';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import { NavigationProp } from "@react-navigation/native";
+import { UserContext } from "../../UserContext";
 import { Block, Text, theme } from "galio-framework";
 import { Images, argonTheme } from "../../constants";
-import { useFocusEffect } from '@react-navigation/native';
-
+import { useFocusEffect } from "@react-navigation/native";
 
 interface AnomalyEvent {
-  anomaly_create_time: string,
-  cctv_id: number,
-  anomaly_save_path: string,
-  anomaly_delete_yn: boolean,
-  log_id: number,
-  anomaly_score: number,
-  anomaly_feedback: boolean,
-  member_id: number,
-  cctv_name: string,
-  cctv_url: string
+  anomaly_create_time: string;
+  cctv_id: number;
+  anomaly_save_path: string;
+  anomaly_delete_yn: boolean;
+  log_id: number;
+  anomaly_score: number;
+  anomaly_feedback: boolean;
+  member_id: number;
+  cctv_name: string;
+  cctv_url: string;
 }
 
 const { width, height } = Dimensions.get("screen");
@@ -29,14 +36,13 @@ interface Tab1ScreenProps {
 }
 
 function formatDateTime(dateTimeString: string): string {
-  
   const date = new Date(dateTimeString);
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
 
   return `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
 }
@@ -47,34 +53,37 @@ export default function Tab1Screen(props: Tab1ScreenProps) {
   const { navigation } = props;
   const [anomalyEvents, setAnomalyEvents] = useState<AnomalyEvent[]>([]);
   const [filteredEvents, setFilteredEvents] = useState<AnomalyEvent[]>([]);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   useFocusEffect(
     React.useCallback(() => {
       const fetchAnomalyEvents = async () => {
         try {
-          const response = await fetch(`http://10.28.224.142:30016/api/v0/cctv/loglist_lookup?member_id=${user}`, {
-            method: "GET",
-            headers: { 'accept': 'application/json' },
-            });
-          console.log('receving data...');
+          const response = await fetch(
+            `http://10.28.224.142:30016/api/v0/cctv/loglist_lookup?member_id=${user}`,
+            {
+              method: "GET",
+              headers: { accept: "application/json" },
+            },
+          );
+          console.log("receving data...");
           const data = await response.json();
-          console.log(response.ok)
-    
+          console.log(response.ok);
+
           if (response.ok) {
             console.log(data.isSuccess);
             console.log(data.result);
             setAnomalyEvents(data.result);
           } else {
-            console.error('API 호출에 실패했습니다:', data);
+            console.error("API 호출에 실패했습니다:", data);
           }
         } catch (error) {
-          console.error('API 호출 중 예외가 발생했습니다:', error);
+          console.error("API 호출 중 예외가 발생했습니다:", error);
         }
       };
-    
+
       fetchAnomalyEvents();
-    }, [user])
+    }, [user]),
   );
 
   useEffect(() => {
@@ -82,42 +91,55 @@ export default function Tab1Screen(props: Tab1ScreenProps) {
   }, [searchText, anomalyEvents]);
 
   const filterEvents = () => {
-    const filtered = anomalyEvents.filter(event => {
-      const formattedTime = formatDateTime(event.anomaly_create_time).toLowerCase();
-      return event.cctv_name.toLowerCase().includes(searchText.toLowerCase()) || formattedTime.includes(searchText.toLowerCase());
+    const filtered = anomalyEvents.filter((event) => {
+      const formattedTime = formatDateTime(
+        event.anomaly_create_time,
+      ).toLowerCase();
+      return (
+        event.cctv_name.toLowerCase().includes(searchText.toLowerCase()) ||
+        formattedTime.includes(searchText.toLowerCase())
+      );
     });
     setFilteredEvents(filtered);
   };
 
   const renderItem = ({ item }: { item: AnomalyEvent }) => (
-    <TouchableOpacity 
-      style={styles.item} 
-      onPress={() => navigation.navigate('LogDetailScreen', 
-      { anomaly_create_time: formatDateTime(item.anomaly_create_time),
-        cctv_id: item.cctv_id,
-        anomaly_save_path: item.anomaly_save_path,
-        anomaly_delete_yn: item.anomaly_delete_yn,
-        log_id: item.log_id,
-        anomaly_score: item.anomaly_score,
-        anomaly_feedback: item.anomaly_feedback,
-        member_id: item.member_id,
-        cctv_name: item.cctv_name,
-        cctv_url: item.cctv_url
-         })}
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() =>
+        navigation.navigate("LogDetailScreen", {
+          anomaly_create_time: formatDateTime(item.anomaly_create_time),
+          cctv_id: item.cctv_id,
+          anomaly_save_path: item.anomaly_save_path,
+          anomaly_delete_yn: item.anomaly_delete_yn,
+          log_id: item.log_id,
+          anomaly_score: item.anomaly_score,
+          anomaly_feedback: item.anomaly_feedback,
+          member_id: item.member_id,
+          cctv_name: item.cctv_name,
+          cctv_url: item.cctv_url,
+        })
+      }
     >
       <Text style={styles.title}>{item.cctv_name}</Text>
-      <Text style={styles.timestamp}>{formatDateTime(item.anomaly_create_time)}</Text>
+      <Text style={styles.timestamp}>
+        {formatDateTime(item.anomaly_create_time)}
+      </Text>
     </TouchableOpacity>
   );
 
   return (
     <ImageBackground
-        source={Images.Onboarding}
-        style={{ width, height, zIndex: 1 }}
-      >
+      source={Images.Onboarding}
+      style={{ width, height, zIndex: 1 }}
+    >
       <View style={{ flex: 1 }}>
         <TextInput
-          style={{...styles.searchInput, backgroundColor: 'white', margin:15}}
+          style={{
+            ...styles.searchInput,
+            backgroundColor: "white",
+            margin: 15,
+          }}
           onChangeText={setSearchText}
           value={searchText}
           placeholder="검색 (CCTV 이름 또는 날짜)"
@@ -125,13 +147,14 @@ export default function Tab1Screen(props: Tab1ScreenProps) {
         <FlatList
           data={filteredEvents}
           renderItem={renderItem}
-          keyExtractor={item => item.log_id.toString()}
+          keyExtractor={(item) => item.log_id.toString()}
           style={{ flex: 1 }}
+          contentContainerStyle={{ paddingBottom: 300 }}
         />
       </View>
     </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   searchInput: {
@@ -140,27 +163,27 @@ const styles = StyleSheet.create({
     paddingLeft: 8,
     margin: 10,
     borderRadius: 10,
-    borderColor: '#CCCCCC',
+    borderColor: "#CCCCCC",
   },
   item: {
-    backgroundColor: '#EEEEEE', // 회색 배경
+    backgroundColor: "#EEEEEE", // 회색 배경
     borderWidth: 1,
-    borderColor: '#CCCCCC', // 테두리 색상
+    borderColor: "#CCCCCC", // 테두리 색상
     borderRadius: 10, // 모서리 둥글게
     padding: 20, // 내부 패딩
     marginVertical: 8,
     marginHorizontal: 16,
-    alignItems: 'flex-start', // 자식 요소들 왼쪽 정렬
+    alignItems: "flex-start", // 자식 요소들 왼쪽 정렬
   },
   title: {
     fontSize: 24, // 제목 폰트 사이즈
-    fontWeight: 'bold', // 글씨 두껍게
+    fontWeight: "bold", // 글씨 두껍게
     marginBottom: 4, // 제목과 날짜/시간 사이의 여백
-      fontFamily: 'C24',
+    fontFamily: "C24",
   },
   timestamp: {
     fontSize: 16, // 날짜/시간 폰트 사이즈
-    color: '#555555', // 날짜/시간 색상
-    fontFamily: 'NGB',
+    color: "#555555", // 날짜/시간 색상
+    fontFamily: "NGB",
   },
 });
--- frontend/front/src/screens/Tab3/Tab3Screen.tsx
+++ frontend/front/src/screens/Tab3/Tab3Screen.tsx
@@ -1,61 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Images } from "../../constants";
+import { NavigationProp } from "@react-navigation/native";
 
+type RootStackParamList = {
+  CctvSettingScreen: undefined;
+  Alarm: undefined;
+  Profile: undefined;
+};
 
 interface Tab2ScreenProps {
-  navigation: NavigationProp<any>;
+  navigation: NavigationProp<
+    RootStackParamList,
+    "CctvSettingScreen" | "Alarm" | "Profile"
+  >;
 }
+
+const { width, height } = Dimensions.get("screen");
 
 export default function Tab2Screen(props: Tab2ScreenProps) {
   const { navigation } = props;
   return (
-    <View style={styles.container}>
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={() => navigation.navigate('CctvSettingScreen', {})}
-      >
-        <Text style={styles.buttonText}>CCTV</Text>
-      </TouchableOpacity>
+    <ImageBackground
+      source={Images.Onboarding}
+      style={{ width, height, zIndex: 1 }}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("CctvSettingScreen", {})}
+        >
+          <Text style={styles.buttonText}>CCTV</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={() => navigation.navigate('Alarm', {})}
-      >
-        <Text style={styles.buttonText}>알림/동영상</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={() => navigation.navigate('Profile', {})}
-      >
-        <Text style={styles.buttonText}>개인 정보</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Alarm", {})}
+        >
+          <Text style={styles.buttonText}>알림/동영상</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Profile", {})}
+        >
+          <Text style={styles.buttonText}>개인 정보</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-};
-  
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8F5E9', // This is a light green background similar to your image
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    backgroundColor: '#FFFFFF', // White color for the button background
+    backgroundColor: "#FFFFFF", // White color for the button background
     padding: 20,
     marginVertical: 10,
     borderRadius: 10,
-    shadowColor: 'rgba(0,0,0, .4)', // Shadow color
+    shadowColor: "rgba(0,0,0, .4)", // Shadow color
     shadowOffset: { height: 1, width: 1 },
     shadowOpacity: 1,
     shadowRadius: 1,
     elevation: 2,
-    width: '80%', // Set width to 80% of the container width
+    width: "80%", // Set width to 80% of the container width
   },
   buttonText: {
     fontSize: 18,
-    color: '#000', // Black color for the text
-    textAlign: 'center',
+    color: "#000", // Black color for the text
+    textAlign: "center",
+    fontFamily: "NGB",
   },
 });
